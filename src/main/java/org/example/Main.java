6059828ba338a554fd3970060346791a52e0127c
Kirill <kaxyonov@gmail.com>
2023-05-20 17:08:21 +0300

    verify

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Ticket ticket = new Ticket("3", "51", 7, 4, 5);
+        Ticket ticket1 = new Ticket("2", "14", 6, 7, 10);
+        Ticket ticket2 = new Ticket("6", "65", 145, 90, 11);
+        Ticket ticket3 = new Ticket("7", "75", 154, 90, 11);
+        Ticket ticket4 = new Ticket("1", "57", 1, 90, 11);
+        Ticket ticket5 = new Ticket("4", "h5", 17, 90, 11);
+        Ticket ticket6 = new Ticket("5", "4342", 97, 90, 11);
+       TicketTimeComparator ticketTimeComparator = new TicketTimeComparator();
+        Ticket[] tic = {ticket1,ticket,ticket2,ticket3,ticket4,ticket5,ticket6};
+        Arrays.sort(tic,ticketTimeComparator);
+
+        }
     }
-}
+
---
+++ src/test/java/AviaSoulsTest.java
@@ -0,0 +1,120 @@
+import org.example.AviaSouls;
+import org.example.Ticket;
+import org.example.TicketTimeComparator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+public class AviaSoulsTest {
+    @Test
+    public void testEquals() {
+        Ticket t1 = new Ticket("Москва", "Берлин", 100, 12, 15);
+        Ticket t2 = new Ticket("Москва", "Берлин", 100, 12, 15);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testCompareToLess() {
+        Ticket t1 = new Ticket("Москва", "Берлин", 100, 12, 15);
+        Ticket t2 = new Ticket("Москва", "Париж", 200, 12, 15);
+        Assertions.assertTrue(t1.compareTo(t2) < 0);
+    }
+
+    @Test
+    void testCompareToGreater() {
+        Ticket t1 = new Ticket("Москва", "Париж", 300, 12, 15);
+        Ticket t2 = new Ticket("Москва", "Берлин", 200, 12, 15);
+        Assertions.assertTrue(t1.compareTo(t2) > 0);
+    }
+
+    @Test
+    void testCompareToEqual() {
+        Ticket t1 = new Ticket("Москва", "Берлин", 300, 12, 15);
+        Ticket t2 = new Ticket("Москва", "Лондон", 300, 12, 15);
+        Assertions.assertEquals(t1.compareTo(t2), 0);
+    }
+
+    @Test
+    void testAdd() {
+        AviaSouls manager = new AviaSouls();
+        Ticket t1 = new Ticket("Москва", "Берлин", 100, 12, 15);
+        Ticket t2 = new Ticket("Москва", "Брюсель", 200, 12, 15);
+        Ticket t3 = new Ticket("Москва", "Париж", 300, 12, 15);
+        manager.add(t1);
+        manager.add(t2);
+        manager.add(t3);
+        Ticket[] expected = {t1, t2, t3};
+        Assertions.assertArrayEquals(manager.findAll(), expected);
+    }
+
+    @Test
+    void testSearch() {
+        AviaSouls manager = new AviaSouls();
+        Ticket t1 = new Ticket("Москва", "Париж", 100, 12, 15);
+        Ticket t2 = new Ticket("Москва", "Брюсель", 200, 12, 15);
+        Ticket t3 = new Ticket("Москва", "Париж", 300, 12, 15);
+        manager.add(t1);
+        manager.add(t2);
+        manager.add(t3);
+        Ticket[] expected = {t1, t3};
+        Assertions.assertArrayEquals(manager.search("Москва", "Париж"), expected);
+    }
+
+    @Test
+    void testSearchAndSortByTicketTimeComparator() {
+        AviaSouls manager = new AviaSouls();
+        Ticket t1 = new Ticket("Москва", "Париж", 800, 11, 19);
+        Ticket t2 = new Ticket("Москва", "Брюсель", 400, 9, 14);
+        Ticket t3 = new Ticket("Москва", "Нью-Йорк", 300, 12, 15);
+        Ticket t4 = new Ticket("Москва", "Лондон", 100, 8, 19);
+        manager.add(t1);
+        manager.add(t2);
+        manager.add(t3);
+        manager.add(t4);
+        Ticket[] result = manager.searchAndSortBy("Москва", "Нью-Йорк", new TicketTimeComparator());
+        Assertions.assertEquals("Москва", result[0].getFrom());
+        Assertions.assertEquals("Нью-Йорк", result[0].getTo());
+        Assertions.assertEquals(12, result[0].getTimeFrom());
+        Assertions.assertEquals(15, result[0].getTimeTo());
+    }
+
+    @Test
+    public void testSearchAndSortByTicketTimeComparatorNoMatches() {
+        AviaSouls manager = new AviaSouls();
+        Ticket t1 = new Ticket("Москва", "Париж", 800, 11, 19);
+        Ticket t2 = new Ticket("Москва", "Брюсель", 400, 9, 14);
+        Ticket t3 = new Ticket("Москва", "Нью-Йорк", 300, 12, 15);
+        Ticket t4 = new Ticket("Москва", "Лондон", 100, 8, 19);
+        manager.add(t1);
+        manager.add(t2);
+        manager.add(t3);
+        manager.add(t4);
+        Ticket[] result = manager.searchAndSortBy("Москва", "Минск", new TicketTimeComparator());
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void testSearchAndSortByTicketTimeComparatorMultipleMatches() {
+        AviaSouls manager = new AviaSouls();
+        Ticket t1 = new Ticket("Москва", "Париж", 800, 11, 19);
+        Ticket t2 = new Ticket("Москва", "Брюсель", 400, 9, 14);
+        Ticket t3 = new Ticket("Москва", "Нью-Йорк", 300, 12, 15);
+        Ticket t4 = new Ticket("Москва", "Париж", 100, 8, 20);
+        manager.add(t1);
+        manager.add(t2);
+        manager.add(t3);
+        manager.add(t4);
+        Ticket[] result = manager.searchAndSortBy("Москва", "Париж", new TicketTimeComparator());
+        Assertions.assertEquals("Москва", result[0].getFrom());
+        Assertions.assertEquals("Париж", result[0].getTo());
+        Assertions.assertEquals(11, result[0].getTimeFrom());
+        Assertions.assertEquals(19, result[0].getTimeTo());
+
+        Assertions.assertEquals("Москва", result[1].getFrom());
+        Assertions.assertEquals("Париж", result[1].getTo());
+        Assertions.assertEquals(8, result[1].getTimeFrom());
+        Assertions.assertEquals(20, result[1].getTimeTo());
+
+    }
+}
